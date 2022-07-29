e814d0764c6fc678405e0e4bf1b079ec023755ed
고현영 <gusdud3890@gmail.com>
2022-07-29 18:58:21 +0900

    음양 더하기 문제

---
+++ 프로그래머스/음양 더하기.py
@@ -0,0 +1,21 @@
+# 어떤 정수들이 있습니다.
+# 이 정수들의 절댓값을 차례대로 담은 정수 배열 absolutes와 이 정수들의 부호를 차례대로 담은 불리언 배열 signs가 매개변수로 주어집니다. 
+# 실제 정수들의 합을 구하여 return 하도록 solution 함수를 완성해주세요.
+
+
+# def solution(absolutes, signs):
+#     signs_dict = zip(absolutes, signs)
+#     sum = 0
+#     for i, j in signs_dict:
+#         if j == True:
+#             sum += i
+#         else:
+#             sum += -i
+
+#     return sum
+
+
+def solution(absolutes, signs):
+    return sum(absolute if sign else -absolute for absolute, sign in zip(absolutes, signs))
+
+print(solution([4, 7, 12], [True, False, True]))
