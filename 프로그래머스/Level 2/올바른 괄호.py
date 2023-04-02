9155a07d7829ed84177fb15f318e937ed4d16605
hyunyoung <gusdud3890@gmail.com>
2023-04-03 07:51:29 +0900

    올바른 괄호

---
+++ 프로그래머스/Level 2/올바른 괄호.py
@@ -0,0 +1,18 @@
+# '(' 또는 ')' 로만 이루어진 문자열 s가 주어졌을 때, 문자열 s가 올바른 괄호이면 true를 return 하고, 올바르지 않은 괄호이면 false를 return 하는 solution 함수를 완성해 주세요.
+
+
+def solution(s):
+    table = {
+        ")" : "("
+    }
+    
+    stack = []
+    
+    for char in s:
+        if char not in table:
+            stack.append(char)
+        elif not stack or table[char] != stack.pop():
+            return False
+    return len(stack) == 0
+
+print(solution("()()"))
